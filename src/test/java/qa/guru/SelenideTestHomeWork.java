d5629d638fe2d0280daf207cf8af627e39a9a755
aleksandrtretyakov <tretyakov.aa74+2@gmail.com>
2022-05-07 21:47:06 +0400

    little improvements

--- src/test/java/qa/guru/SelenideTestHomeWork.java
+++ src/test/java/qa/guru/SelenideTestHomeWork.java
@@ -2,15 +2,10 @@ package qa.guru;
 
 import com.codeborne.selenide.Condition;
 import com.codeborne.selenide.Selenide;
-import org.junit.jupiter.api.Assertions;
-import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
-
-import java.lang.module.Configuration;
 
 import static com.codeborne.selenide.Selectors.withTagAndText;
 import static com.codeborne.selenide.Selenide.$;
-import static com.codeborne.selenide.Selenide.$$;
 
 public class SelenideTestHomeWork {
 //    Task1
@@ -20,18 +15,14 @@ $("h1 div") - h1 и div - элементы ищутся совместно и в
 $("h1").$("div") - после того как будет найден h1-элемент, будет выполнен поиск первого по списку div-элемента.
 */
 
-
+    //    Task2
     @Test
-    void SelenideSoftAssertions () {
-//    Task2
+    void SelenideSoftAssertions() {
 
         Selenide.open("https://github.com/selenide/selenide");
         $("#wiki-tab").click();
         $(withTagAndText("a", "Soft assertions")).shouldBe(Condition.visible).click();
-//        $("#wiki-body > div.markdown-body > ul:nth-child(3) > li:nth-child(6) > a").shouldBe(Condition.visible);
         $("#user-content-3-using-junit5-extend-test-class").shouldBe(Condition.visible);
-
-
 
     }
 }
